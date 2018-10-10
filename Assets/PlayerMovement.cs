5811a1af630f58d7ed487f8461556ebb91ec4947
Gaurav Nanda <gauravnba@gmail.com>
2018-10-10 13:10:41 +0530

    Initial commit with working player controller

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    float MaxSpeed = 0.01f;
+    [SerializeField]
+    float TiltRotation = 10.0f;
+
+    readonly float MaximumDisplacement = 0.9f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float movementDirection = Input.GetAxisRaw("Vertical");
+        Vector3 newPosition = transform.position + new Vector3(0, 0,  movementDirection * MaxSpeed);
+        newPosition.z = (Mathf.Abs(newPosition.z) > MaximumDisplacement) ? MaximumDisplacement * movementDirection : newPosition.z;
+
+        transform.position = newPosition;
+        transform.rotation = Quaternion.AngleAxis(movementDirection * TiltRotation, Vector3.right);
+    }
+}
